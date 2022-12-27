5202b9113db0c41f4ed06a69006beb4c51b6dc0c
xiaoxu <18320686056@163.com>
2022-12-27 16:07:06 +0800

    new spring boot project add pom.xml

---
+++ src/main/java/cn/xiaoxu/intelligencesql/MainApplication.java
@@ -0,0 +1,14 @@
+package cn.xiaoxu.intelligencesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class MainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/intelligencesql/IntelligenceSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intelligencesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
